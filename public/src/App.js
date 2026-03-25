477f59ca9bd25e89a384a4c96c3a95f5dc1b1eca
tal-saj <talalkhan4860@gmail.com>
2026-03-25 20:18:46 +0500

    friends page attempted fix 6

--- public/src/App.js
+++ public/src/App.js
@@ -11,25 +11,31 @@ import ProtectedRoute from "./components/ProtectedRoute";
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/setAvatar" element={<SetAvatar />} />
-        
-        {/* Protected Routes */}
-        <Route path="/" element={
-          <ProtectedRoute>
-            <Chat />
-          </ProtectedRoute>
-        } />
-        <Route path="/friends" element={
-          <ProtectedRoute>
-            <FriendsPage />
-          </ProtectedRoute>
-        } />
-        
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+    <Routes>
+  <Route path="/login" element={<Login />} />
+  <Route path="/register" element={<Register />} />
+  <Route path="/setAvatar" element={<SetAvatar />} />
+  
+  {/* Add a unique key to the fragment or the route to force a clean swap */}
+  <Route 
+    path="/" 
+    element={
+      <ProtectedRoute key="chat-route">
+        <Chat />
+      </ProtectedRoute>
+    } 
+  />
+  <Route 
+    path="/friends" 
+    element={
+      <ProtectedRoute key="friends-route">
+        <FriendsPage />
+      </ProtectedRoute>
+    } 
+  />
+  
+  <Route path="*" element={<Navigate to="/" replace />} />
+</Routes>
     </BrowserRouter>
   );
 }
