b8243135a7c97a7e133beca31fd20dbc5a7a7b11
tal-saj <talalkhan4860@gmail.com>
2026-03-25 19:36:07 +0500

    friends button fixed 2

--- public/src/components/Contacts.jsx
+++ public/src/components/Contacts.jsx
@@ -1,6 +1,6 @@
 // Contacts.jsx
 import React, { useState, useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Logo from "../assets/logo.svg";
 import Logout from "./Logout";
 
